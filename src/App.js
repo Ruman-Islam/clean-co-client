82d9dc76cfde972167f0515e94727ec7307943ea
Ruman-Islam <rumanislam48@gmail.com>
2022-05-13 21:33:14 +0600

    dark mode implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold text-success">Clean Co</h1>
-    </div>
+    <>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Navbar>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        mytheme: {
+        light: {
           "primary": "#176F6B",
           "secondary": "#FFC000",
           "accent": "#FFEDD5",
@@ -17,6 +17,7 @@ module.exports = {
           "error": "#FA5C5C",
         },
       },
+      "dark",
     ],
   },
   plugins: [require("daisyui")],
